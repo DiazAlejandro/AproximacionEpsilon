3deca386bcbc2c1b3cdcbf9cc5e5d2f9f4d7cfeb
DiazAlejandro <alejandro_diaz1253@hotmail.com>
2022-06-15 10:01:44 -0500

    Actualizacion

--- src/main/java/com/mycompany/aproximacionepsilon/AproximacionEpsilon.java
+++ src/main/java/com/mycompany/aproximacionepsilon/AproximacionEpsilon.java
@@ -2,7 +2,6 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
  */
-
 package com.mycompany.aproximacionepsilon;
 
 /**
@@ -11,7 +10,27 @@ package com.mycompany.aproximacionepsilon;
  */
 public class AproximacionEpsilon {
 
+    public static double val_x(double epsilon, double inf, double sup) {
+        double x = (inf + sup) / 2; //Mitad del limite
+        double y = evaluar(x); //Valor en y 
+        double valx = 0;
+        double x1 = 0, y1 = 0;
+        for (double i = inf ; i <= x ; i+=epsilon){
+            valx = evaluar(i);
+            //System.out.println("x = "+i+ " y = "+valx);
+            x1 = i;
+            y1 = valx;
+        }
+        System.out.println("Valor x = " + x);
+        System.out.println("Valor de x = "+x1+" Valor de y = "+y1);
+        return valx;
+    }
+
+    public static double evaluar(double x) {
+        return Math.pow(x, 2);
+    }
+
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        val_x(0.00001, 0.5, 0.75);
     }
 }
---
+++ src/main/java/com/mycompany/aproximacionepsilon/Seno.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.aproximacionepsilon;
+
+/**
+ *
+ * @author aleja
+ */
+public class Seno {
+    public static void main (String [] args){
+    
+    }
+}
